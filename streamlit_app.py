ad0ca7b234a7ef448ad87c5baf5c9c49143e4f74
CSBaruh <cemsbaruh@hotmail.com>
2025-04-04 23:46:34 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.title('🌐 Machine Learning App')
 
 st.info('This app buildes a machine learning model!')
 
-df = pd.read.csv('https://raw.githubusercontent.com/CSBaruh/Datas/refs/heads/main/synthetic_online_retail_data.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/CSBaruh/Datas/refs/heads/main/synthetic_online_retail_data.csv')
 df
